e32a010fc1366d4ac0b5da5284a4de06c2dde404
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-05-16 08:46:15 -0400

    fix: Apparently `STEP_NAME` does not have to be set

--- scripts/sanitize-name.sh
+++ scripts/sanitize-name.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-: "${STEP_NAME:?Environment variable STEP_NAME must be set}"
 : "${GITHUB_OUTPUT:?Environment variable GITHUB_OUTPUT must be set}"
 
-name="$(echo "$STEP_NAME" | sed -E 's/([a-z])([A-Z])/\1 \2/g' | xargs)"
+name="$(echo "${STEP_NAME:-}" | sed -E 's/([a-z])([A-Z])/\1 \2/g' | xargs)"
 echo "formatted_name=$name" >> "$GITHUB_OUTPUT"
